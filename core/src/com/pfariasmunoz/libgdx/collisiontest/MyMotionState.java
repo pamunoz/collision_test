cb688348e641b39a4cdd75c25f1f3e871bd990e9
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-15 11:56:33 -0300

    add custom class to update render instance acord bullet

---
+++ core/src/com/pfariasmunoz/libgdx/collisiontest/MyMotionState.java
@@ -0,0 +1,26 @@
+package com.pfariasmunoz.libgdx.collisiontest;
+
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.physics.bullet.linearmath.btMotionState;
+
+/**
+ * Created by Pablo Farias on 15-08-16.
+ */
+public class MyMotionState extends btMotionState {
+    final ModelInstance instance;
+
+    public MyMotionState(ModelInstance instance) {
+        this.instance = instance;
+    }
+
+    @Override
+    public void getWorldTransform(Matrix4 worldTrans) {
+        worldTrans.set(instance.transform);
+    }
+
+    @Override
+    public void setWorldTransform(Matrix4 worldTrans) {
+        instance.transform.set(worldTrans);
+    }
+}
